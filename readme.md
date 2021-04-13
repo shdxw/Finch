921a2328cf3a2488033a3a8fc01a25428b236098
shdxw <56305521+shdxw@users.noreply.github.com>
2021-04-14 00:06:06 +1000

    init commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+1) git clone 
+2) отдельно докачиваем https://www.birdbraintechnologies.com/downloads/finch/FinchJavaCommandLine.zip
+3) в проекте нажимаем ctrl+alt+shift+s  добавляем библиотеки из папки libs в наш проект
---
+++ src/main/java/Code/graphics/GestureProgrammer/GestureProgrammerUtils/GestureCommand.java
@@ -0,0 +1,38 @@
+package Code.graphics.GestureProgrammer.GestureProgrammerUtils;
+
+
+import com.birdbraintechnologies.Finch;
+
+/**
+ * @author Alex Styler (astyler@gmail.com)
+ */
+
+public class GestureCommand
+   {
+   public double leftVel = 0;
+   public double rightVel = 0;
+
+   public GestureCommand(double left, double right)
+      {
+      this.leftVel = left;
+      this.rightVel = right;
+      }
+
+   public void execute(Finch finch)
+      {
+      finch.setWheelVelocities((int)leftVel, (int)rightVel);
+      }
+
+   public String toString()
+      {
+      return "(" + leftVel + ", " + rightVel + ")";
+      }
+
+   public static class StopCommand extends GestureCommand
+      {
+      public StopCommand()
+         {
+            super(0.0,0.0);            
+         }
+      }
+   }
